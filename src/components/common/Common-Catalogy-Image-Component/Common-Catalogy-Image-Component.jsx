94812171dad35dae7a1c4dd1c21b1b0a6fd6cc92
duy366110 <duy366110@gmail.com>
2024-01-03 12:04:12 +0700

    #Commit 03/01/2023: Config template image category and service delete image featured.

--- src/components/common/Common-Catalogy-Image-Component/Common-Catalogy-Image-Component.jsx
+++ src/components/common/Common-Catalogy-Image-Component/Common-Catalogy-Image-Component.jsx
@@ -1,40 +1,39 @@
-import React from 'react';
-// import { useNavigate } from "react-router-dom";
-// import useHttp from '../../../hook/use-http';
-// import config from "../../../configs/config.env";
+import useHttp from '../../../hook/use-http';
+import config from "../../../configs/config.env";
 import classes from "./Common-Catalogy-Image-Component.module.css";
 
 const CommonCatalogyImageComponent = (props) => {
-    // const navigate = useNavigate();
-
-    // const { httpMethod } = useHttp();
+    const { httpMethod } = useHttp();
 
     // PHƯƠNG THỨC THỰC HIỆN XOÁ ẢNH
-    // const deleteImage = async (event) => {
-    //     if(window.confirm("Are you sure delete image")) {
-    //         let { img } = event.target.dataset;
-    //         httpMethod({
-    //             url: `${config.URI}/api/admin/${props.endpoint}/photo`,
-    //             method: 'DELETE',
-    //             author: '',
-    //             payload: JSON.stringify({id: props?.id, photo: img}),
-    //             customForm: false
-    //         },
-    //         (infor) => {
+    const onDeleteImageHandler = async (event) => {
+        if(window.confirm("Are you sure delete image")) {
+            let image = event.target.src;
+            httpMethod({
+                url: `${config.URI}/api/admin/${props.endpoint}/photo`,
+                method: 'DELETE',
+                author: '',
+                payload: JSON.stringify({id: props.id, photo: image}),
+                customForm: false
+            },
+            (infor) => {
 
-    //             let { status } = infor;
-    //             if(status) {
-    //                 navigate(`/${props.endpoint}s`);
-    //             }
-    //         })
-    //     }
-    // }
+                let { status } = infor;
+                if(status) {
+                   window.location.reload();
+                }
+            })
+        }
+    }
     
     return (
         <div className={classes['common-catalogy-image-component']}>
             {props.thumbnails.map((photo, index) => {
                 return (
-                    <img key={index} src={photo} alt="Thumbnail" />
+                    <img
+                        onClick={onDeleteImageHandler}
+                        key={index}
+                        src={photo} alt="Thumbnail" />
                 )
             })}
         </div>
